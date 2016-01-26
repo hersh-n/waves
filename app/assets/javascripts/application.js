1424505183570830203721e5434953a226b2444c
Hershel Nashman <hershel.nashman@gmail.com>
2016-01-26 01:14:10 -0500

    navbar transparent when scrolling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,4 +17,32 @@
 //= require_tree .
 
 
+// var a = $(".nav").offset().top;
 
+// $(document).scroll(function(){
+//     if($(this).scrollTop() > a){
+//        $('.navbar').animate({backgroundColor: 'rgba(34, 34, 34, 0.6)'}, 3000);
+//     } else {
+//        $('.navbar').css({"background":"#222"});
+//     }
+// });
+
+var a = $(".nav").offset().top;
+
+$(document).scroll(function(){
+    if($(this).scrollTop() > a)
+    {
+       $('.navbar').css({"backgroundColor":"rgba(34, 34, 34, 0.6)"});
+    } else {
+       $('.navbar').css({"backgroundColor":"rgba(34, 34, 34, 1.0)"});
+    }
+});
+
+$(function() {
+   $("li").click(function() {
+      // remove classes from all
+      $("li").removeClass("active");
+      // add class to the one we clicked
+      $(this).addClass("active");
+   });
+});
