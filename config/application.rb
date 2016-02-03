2d07d67cdb0caca4e5d97d42602a3e7cec551e21
Hershel Nashman <hershel.nashman@gmail.com>
2016-02-02 22:53:41 -0500

    video added as background image

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Waves
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << Rails.root.join("/app/assets/videos")
   end
 end
